3500cc530fc79b466eb8eba26a360bbfd76fd1fd
menduvada <vptl1885@gmail.com>
2026-08-15 11:32:30 +0530

    fix: explicit Capitalized imports in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header.jsx";
-import Footer from "./components/Footer.jsx";
-import Dashboard from "./pages/Dashboard.jsx";
-import ProductDetailPage from "./pages/ProductDetailPage.jsx";
-import CartPage from "./pages/CartPage.jsx";
-import WishlistPage from "./pages/WishlistPage.jsx";
-import LoginPage from "./pages/LoginPage.jsx";
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Dashboard from './pages/Dashboard'
+import ProductDetailPage from './pages/ProductDetailPage'
+import CartPage from './pages/CartPage'
+import WishlistPage from './pages/WishlistPage'
+import LoginPage from './pages/LoginPage'
 
 export default function App() {
   return (
@@ -25,5 +25,5 @@ export default function App() {
         <Footer />
       </div>
     </Router>
-  );
+  )
 }
